1499b19cca82303262e3509bde8badd01af05d49
Alfredo Barrón Rodríguez <alfreedobarron@gmail.com>
2015-06-10 08:34:44 -0500

    config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 session_cache_limiter(false);
 session_start();
-date_default_timezone_set('America/Mexico_City');
+//date_default_timezone_set('America/Mexico_City');
 // CONFIG
 define('SYS_TITLE', 'Rally TIC');
 define('SYS_NAME', 'Rally TIC');
