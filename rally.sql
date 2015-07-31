dfc2767b6277cee3c7848d3dd28753a453a8dbc7
Alfredo Barrón Rodríguez <alfreedobarron@gmail.com>
2015-07-31 11:04:32 -0500

    Respaldo

--- rally.sql
+++ rally.sql
@@ -1,4 +1,3 @@
---User poner un status para quien va registrar los integrantes
 create table users(
   id serial primary key,
   username varchar(25) not null,
@@ -12,9 +11,8 @@ create table users(
   updated_at timestamp not null default now()
 );
 
-INSERT INTO users(username,email,password,rol) VALUES ('alfredobarron','alfreedobarron@gmail.com','afec8e3faf8cc984cf3e0060e73fb945','P'),('j2deme','j2deme@gmail.com','827ccb0eea8a706c4c34a16891f84e7b','O');
+INSERT INTO users(username,email,password,rol) VALUES ('alfredobarron','alfreedobarron@gmail.com','afec8e3faf8cc984cf3e0060e73fb945','P'),('j2deme','j2deme@gmail.com','827ccb0eea8a706c4c34a16891f84e7b','O'),('nitgard','nitgard.zapata@tecvalles.mx','ef6299c9e7fdae6d775819ce1e2620b8','O');
 
---No más de 4 integrantes o individuales
 create table teams(
   id serial primary key,
   name varchar(25) not null,
@@ -24,7 +22,7 @@ create table teams(
   updated_at timestamp not null default now()
 );
 
-INSERT INTO teams(name,password) VALUES ('Alfa','1234'),('Beta','4321'),('Omega','1122'),('Zeta','5678'),('Gamma','5690');
+INSERT INTO teams(name,password) VALUES ('Alfa','1234'),('Beta','4321'),('Omega','1122'),('Zeta','5678'),('Gamma','5690'),('Equipo ITV','prueba');
 
 create table team_user(
   id serial primary key,
@@ -33,8 +31,6 @@ create table team_user(
   created_at timestamp not null default now(),
   updated_at timestamp not null default now()
 );
-
---Usuarios listos
 
 create table events(
   id serial primary key,
@@ -46,6 +42,9 @@ create table events(
   updated_at timestamp not null default now()
 );
 
+INSERT INTO events(name,description,date_event,hour_start) VALUES ('La fantastica aventura','Vamos a buscar las esferas del dragón',2015-07-24,'15:00'),('Evento de Prueba','Prueba del sistema',2015-07-25,'08:00');
+)
+
 create table questions(
   id serial primary key,
   message text,
@@ -54,6 +53,8 @@ create table questions(
   created_at timestamp not null default now(),
   updated_at timestamp not null default now()
 );
+
+INSERT INTO questions (message,answer,track) VALUES ('Esfera del dragón de 1 estrella','1','Una'),('Esfera del dragón de 2 estrellas','2','Dos'),('¿De qué color es el caballo blanco de Napoleon?','Blanco','#FFFFFF');
 
 create table stations(
   id serial primary key,
@@ -64,17 +65,21 @@ create table stations(
   updated_at timestamp not null default now()
 );
 
+INSERT INTO stations (name,lat,lng) VALUES ('Estacionamiento del CINVESTAV','23.720849550243056','-99.07793238759041'),('Fuente del CINVESTAV','23.720553649386304','-99.07794445753098'),('Tec Valles','22.021999431637663','-99.03582572937012'),('Casa Victoria','23.745316830149086','-99.15129834786057');
+
 create table activities(
   id serial primary key,
-  name text, --A,B,C
-  station_id int, --Estacion
-  question_id int, --Pregunta
-  intents int, --Numero de intentos
-  time int, --No sobrepasar los 5 minutos
-  penalty boolean default false, --Castigo
+  name text,
+  station_id int,
+  question_id int,
+  intents int,
+  time int,
+  penalty boolean default false,
   created_at timestamp not null default now(),
   updated_at timestamp not null default now()
 );
+
+INSERT INTO activities (name,station_id,question_id,intents,time,penalty) VALUES ('Actividad A',2,1,2,1,1),('Actividad B',1,2,0,0,0),('Actividad Valles',3,3,5,5,1),('Actividad Prueba',4,2,2,0,1);
 
 create table activity_event(
   id serial primary key,
@@ -84,6 +89,8 @@ create table activity_event(
   updated_at timestamp not null default now()
 );
 
+INSERT INTO activity_event(event_id,activity_id) VALUES (1,1),(1,2),(2,1),(2,1),(1,4);
+
 create table event_team(
   id serial primary key,
   event_id int not null,
@@ -92,19 +99,4 @@ create table event_team(
   updated_at timestamp not null default now()
 );
 
---Pendiente
-create table bitacora(
-  id serial primary key,
-  team_id int not null,
-  activity_id int not null,
-  event_id int not null,
-  lat text not null,
-  lng text not null,
-  time text not null,
-
-  created_at timestamp not null default now(),
-  updated_at timestamp not null default now(),
-  foreign key (user_id) references users(id),
-  foreign key (event_id) references events(id),
-  foreign key (activity_id) references activities(id)
-);
+INSERT INTO event_team(event_id,team_id) VALUES (1,1),(2,6),(2,1),(2,2),(2,1),(6,2);
